15027e202ed18dede4ef4e0451fe66f99b673dfd
Free Domain Projects <134573460+FreeDomainProjects@users.noreply.github.com>
2023-05-25 11:03:49 +0700

    update new.js

---
+++ new.js
@@ -0,0 +1,21 @@
+// Create Your Free Domain In Our Server
+
+{
+  "subDomain":"example.my.id", //hanya domain tertentu
+  "dns1":"185.199.108.153", //dns nya
+  "dns2":"185.199.109.153", //dns nya
+  "dns3":"185.199.110.153", //dns nya
+  "dns4":"185.199.111.153", //dns nya
+  "sertificate":"true", //ssl nya
+  "type":"my.id", //jangan diubah yak
+  "server":"ns.my.id", //jangan diubah yak
+}
+{
+  "github":"@FreeDomainProject"
+  "urlGit":"https://github.com/FreeDomainProject"
+  "apikey":"" //Pakai apikey punya anda sendiri
+}
+{
+  // Jangan asal ubah kalau gatau fungsi nya
+  // Don't just change it if you don't know its function
+}
